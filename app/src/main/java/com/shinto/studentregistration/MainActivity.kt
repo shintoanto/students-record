77c0b68bd07f827200a357cacf0e210333d9d90e
tutoojoe <tutoojoephilip@gmail.com>
2022-06-08 12:35:39 +0530

    onTextChangeListner comments removed.

--- app/src/main/java/com/shinto/studentregistration/MainActivity.kt
+++ app/src/main/java/com/shinto/studentregistration/MainActivity.kt
@@ -58,6 +58,7 @@ class MainActivity : AppCompatActivity(), NoteClickInterface, NoteClickDeleteInt
                 if (query != null) {
                     getStudentData(query)
                 }
+
                 return true
             }
 
